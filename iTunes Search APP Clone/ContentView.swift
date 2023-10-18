5bb8ebb42e97ff3f65152f6331519ffecf28d4b6
ElmahmoudOmar <elmahmoud22omar@gmail.com>
2023-10-18 13:44:42 +0100

    Initial Commit

---
+++ iTunes Search APP Clone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iTunes Search APP Clone
+//
+//  Created by macbook pro on 18/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iTunes Search APP Clone/iTunes_Search_APP_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunes_Search_APP_CloneApp.swift
+//  iTunes Search APP Clone
+//
+//  Created by macbook pro on 18/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct iTunes_Search_APP_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
